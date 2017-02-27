52a83728b24e4ef2746b5525afd7c66a619d1ade
TTT <TTT>
2017-02-27 10:08:52 -0500

    my second commit

--- app/src/main/java/com/strobertchs/javafirstcontact/MainActivity.java
+++ app/src/main/java/com/strobertchs/javafirstcontact/MainActivity.java
@@ -2,12 +2,29 @@ package com.strobertchs.javafirstcontact;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Can you see me", Toast.LENGTH_SHORT).show();
+        Log.i("info", "Done creating the app");
+    }
+
+    public void topClick(View v){
+        Toast.makeText(this, "Top button clicked",
+                Toast.LENGTH_SHORT).show();
+        Log.i("info","The user clicked the top button");
+    }
+    public void bottomClick(View v){
+        Toast.makeText(this, "Bottom button clicked",
+                Toast.LENGTH_SHORT).show();
+        Log.i("info","The user clicked the bottom button");
     }
 }
